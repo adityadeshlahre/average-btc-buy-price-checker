cd1a26bf529de02f4a3b576eadca8f89addb5530
Aditya @ArchLinux <132184385+adityadeshlahre@users.noreply.github.com>
2025-05-21 13:34:22 +0530

    fix: api key error maybe this can fix it try 01

--- api/get-btc-history.ts
+++ api/get-btc-history.ts
@@ -4,6 +4,17 @@ import axios from 'axios';
 export default async function handler(req: VercelRequest, res: VercelResponse) {
 	const { time_start, time_end } = req.query;
 
+	if ( !time_start || !time_end) {
+      return res.status(400).json({ error: "Missing query parameters" });
+    }
+
+	const apiKey = process.env.COINMARKETCAP_API_KEY;
+
+    if (!apiKey) {
+      console.error("API Key is missing");
+      return res.status(500).json({ error: "API Key not found in environment variables" });
+    }
+
 	try {
 		const response = await axios.get(
 			`https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/historical`,
@@ -15,14 +26,14 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
 					time_end,
 				},
 				headers: {
-					'X-CMC_PRO_API_KEY': process.env.COINMARKETCAP_API_KEY!,
+					'X-CMC_PRO_API_KEY': apiKey,
 				},
 			}
 		);
 
 		res.status(200).json(response.data);
 	} catch (error: any) {
-		console.error(error);
-		res.status(500).json({ error: 'Internal Server Error' });
-	}
+    console.error("Error in API route:", error.message);
+    res.status(500).json({ error: "Internal server error", details: error.message });
+  }
 }
